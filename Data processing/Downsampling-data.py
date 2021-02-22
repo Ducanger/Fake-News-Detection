e6b0fe592128479b606fcf2ef1ef24da0cc38490
Đỗ Thành Đạt <71697123+Ducanger@users.noreply.github.com>
2021-02-22 09:44:03 +0700

    Update Downsampling-data.py

--- Data processing/Downsampling-data.py
+++ Data processing/Downsampling-data.py
@@ -1,12 +1,11 @@
-# clean data
 import pandas as pd
 import re
+
 df = pd.read_csv(r'D:\Dat\Fake News Detection\Data\pre-train-data.csv') 
 
 count1 = 0
 for i in range (0, len(df)):
     if df.label[i] == 1: count1 = count1 + 1
-
 
 data = {'post_message': ['Test'],
 	    'label': [0],}
@@ -23,7 +22,9 @@ for i in range (0, len(df)):
     else:
         new_row = {'post_message': df.post_message[i], 'label': df.label[i]}
         df_export = df_export.append(new_row, ignore_index=True)
+	
 df_export = df_export.drop(df_export.index[0])
 print(len(df_export))
+
 # export file
 df_export.to_csv(r"D:\Dat\Fake News Detection\Data\dataBERT.csv")
